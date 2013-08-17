6284a3d855c5d56960e6941a5baca13df94fa6cb
Yak! <yak_ex@mx.scn.tv>
2013-08-17 12:56:45 +0900

    Add per-user conf configuration and adjust size and layout.

--- resource.h
+++ resource.h
@@ -39,6 +39,7 @@
 #define IDC_GUIDE_STATIC                1030
 #define IDC_TOPMOST_CHECK               1031
 #define IDC_7ZVERSION                   1032
+#define IDC_PERUSERCONF_CHECK           1033
 #define IDM_TOPMOST                     40000
 
 // Next default values for new objects
@@ -47,7 +48,7 @@
 #ifndef APSTUDIO_READONLY_SYMBOLS
 #define _APS_NEXT_RESOURCE_VALUE        106
 #define _APS_NEXT_COMMAND_VALUE         40002
-#define _APS_NEXT_CONTROL_VALUE         1033
+#define _APS_NEXT_CONTROL_VALUE         1034
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
 #endif
